9676e421eeac0c574e9de2598d2f05fb7dcb5eaa
Yoon Suhwan <s-yoon@ankh-systems.co.jp>
2026-03-10 10:20:38 +0900

    dev, prod env account mail activation feature

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,21 +53,24 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # FIXME: Replace with your actual Render URL (e.g., 'your-app-name.onrender.com')
+  config.action_mailer.default_url_options = { host: "https://rails-tutorial-2n0z.onrender.com", protocol: "https" }
 
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  # Specify outgoing SMTP server. Remember to add gmail/* credentials via bin/rails credentials:edit.
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            Rails.application.credentials.dig(:gmail, :username),
+    password:             Rails.application.credentials.dig(:gmail, :password),
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
